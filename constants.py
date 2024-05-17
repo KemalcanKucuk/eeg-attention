bd5b92133e48295362f6b4fb0d34f5fb3d0ddc1f
KemalcanKucuk <kkucuk19@ku.edu.tr>
2024-05-17 23:18:05 +0300

    updated channel name

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 import utils
 
-ALL_CHANNELS = ["Fp1", "Fp2", "F7", "F3", "Fz", "F4", "F8", "FT7", "FC3", "FCz", "FC4", "FT8", "T3", "C3", "Cz", "C4", "T4", "TP7", "CP3", "CPz", "CP4", "TP8", "T5", "P3", "Pz", "P4", "T6", "O1", "Oz" , "O2"]
+ALL_CHANNELS = ['Fp1', 'Fp2', 'F7', 'F3', 'Fz', 'F4', 'F8', 'FT7', 'FC3', 'FCz', 'FC4', 'FT8', 'T3', 'C3', 'Cz', 'C4', 'T4', 'TP7', 'CP3', 'CPz', 'CP4', 'TP8', 'T5', 'P3', 'Pz', 'P4', 'T6', 'O1', 'Oz' , 'O2']
 ALL_MODELS = ['K-NN', 'SVM', 'DTC', 'RFC', 'LR', 'NN', 'XGB']
 SELECTED_CHANNELS = ['CP3', 'Cz', 'CPz', 'P3']
 MAIN_CSV_FILE = 'data/eeg_features.csv'
